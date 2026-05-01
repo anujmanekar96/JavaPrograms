d2f1bf80960da7d03ab898875865b830306d42d4
Anuj Manekar <anuj.manekar@technia.com>
2026-05-01 21:30:47 +0530

    Math

---
+++ src/Apr/Loop1.java
@@ -0,0 +1,22 @@
+package Apr;
+
+public class Loop1 {
+    public static void main(String[] args) {
+        for(int i=1; i<=10; i++){
+         //   System.out.println(i);
+        }
+
+        for(int j=10; j>=1; j--){
+         //   System.out.println(j);
+
+
+        }
+        int fact =1;
+        for (int k=1;k<=6; k++){
+
+            fact= fact*k;
+
+        }
+        System.out.println(fact);
+    }
+}

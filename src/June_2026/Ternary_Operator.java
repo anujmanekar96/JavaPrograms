4b635216f1a8089adf997c8e0c10dabd09abad71
Anuj Manekar <anuj.manekar@technia.com>
2026-06-18 21:39:32 +0530

    TernaryOperator

---
+++ src/June_2026/Ternary_Operator.java
@@ -0,0 +1,17 @@
+package June_2026;
+
+public class Ternary_Operator {
+    public static void main(String[] args) {
+       // int a1= (10>34) ? 12:344;
+        
+        int salary1= -44343;
+        int salary2=443;
+        int c = (salary1>salary2) ? salary1:salary2;
+        int d = (salary1<salary2) ? salary1:salary2;
+        System.out.println(c); 
+        System.out.println(d); 
+        
+        
+    }
+    }
+

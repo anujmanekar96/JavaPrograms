7027fe2967654013880ec1fb0ec3ff995902d4ae
Anuj Manekar <anuj.manekar@technia.com>
2026-06-20 13:38:47 +0530

    Break

---
+++ src/June_2026/Break.java
@@ -0,0 +1,18 @@
+package June_2026;
+
+
+public class Break {
+
+    public static void main(String[] args) {
+
+        for (int a = 1; a <= 13; a++) {
+
+            if (a == 5) {
+                break;
+            }
+
+            System.out.println(a);
+        }
+
+    }
+}

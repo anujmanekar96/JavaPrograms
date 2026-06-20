f41aeb1f526a2307d85805203e59be25eb1faa01
Anuj Manekar <anuj.manekar@technia.com>
2026-06-20 11:53:25 +0530

    Loop

---
+++ src/June_2026/Series.java
@@ -0,0 +1,15 @@
+package June_2026;
+
+public class Series {
+    public static void main(String[] args) {
+        for(int a=1; a<=10; a++)
+        {
+            if(a == 8){
+                System.out.println("Eight");
+            }
+            else {
+                System.out.println(a);
+            }
+        }
+    }
+}

b9d0d8af767afff393064a5b276143d8668d1175
Anuj Manekar <anuj.manekar@technia.com>
2026-06-20 23:22:37 +0530

    While_Loop

---
+++ src/June_2026/ContineEvenOdd.java
@@ -0,0 +1,12 @@
+package June_2026;
+
+public class ContineEvenOdd {
+    public static void main(String[] args) {
+        for(int i=1; i<=50; i++){
+            if(i%2 !=0){
+               continue;
+            }
+            System.out.println(i);
+        }
+    }
+}
---
+++ src/June_2026/whileloop.java
@@ -0,0 +1,11 @@
+package June_2026;
+
+public class whileloop {
+    public static void main(String[] args) {
+        int a=1;
+        while(a<=10){
+            System.out.println(a +  " Anuj");
+            a++;
+        }
+    }
+}

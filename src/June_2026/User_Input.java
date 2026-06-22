893e1ab6ac0654b1abe56bf1660d3ae6f2f8cb7d
Anuj Manekar <anuj.manekar@technia.com>
2026-06-22 12:19:32 +0530

    Factorial

---
+++ src/June_2026/User_Input.java
@@ -0,0 +1,17 @@
+package June_2026;
+
+import java.util.Scanner;
+
+public class User_Input {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Number?");
+        int num = sc.nextInt();
+        
+        System.out.println(num);
+        System.out.println("Name?");
+        String name = sc.next();
+        
+       // System.out.println("name");
+    }
+}
--- src/June_2026/whileloop.java
+++ src/June_2026/whileloop.java
@@ -2,10 +2,10 @@ package June_2026;
 
 public class whileloop {
     public static void main(String[] args) {
-        int a=1;
-        while(a<=10){
-            System.out.println(a +  " Anuj");
-            a++;
+        int a=5;
+        while(a>=1){
+            System.out.println(a);
+            a--;
         }
     }
 }

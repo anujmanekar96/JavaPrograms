04ca7dc0919b7a4adf0f698b9305694254c839de
Anuj Manekar <anuj.manekar@technia.com>
2026-06-22 23:49:29 +0530

    Function

---
+++ src/June_2026/Function.java
@@ -0,0 +1,16 @@
+package June_2026;
+
+public class Function 
+{ 
+    public static void main(String[] args) {
+       for(int i=1; i<=5; i++ ){
+           //System.out.println()
+           anuj();
+           
+       }
+    }
+    public static void anuj(){
+        System.out.println("New Function");
+    }
+    
+}
---
+++ src/June_2026/SumofInt.java
@@ -0,0 +1,14 @@
+package June_2026;
+
+public class SumofInt {
+    public static void main(String[] args) {
+        int a=100;
+        int num=100;
+        while(a>=1){
+            num=num+a;
+            System.out.println(num);
+            a--;
+        }
+        
+    }
+}

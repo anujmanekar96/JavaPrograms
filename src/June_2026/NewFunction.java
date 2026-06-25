de9c44c721d7717cde1f7332a075b26056b73304
Anuj Manekar <anuj.manekar@technia.com>
2026-06-25 13:21:54 +0530

    NewFunction

---
+++ src/June_2026/NewFunction.java
@@ -0,0 +1,12 @@
+package June_2026;
+
+public class NewFunction {
+    public static void main(String[] args) {
+        int a=102;
+        print();
+        System.out.println(a);
+    }
+    public static void print(){
+        System.out.println("hello");
+    }
+}
---
+++ src/June_2026/Returntype.java
@@ -0,0 +1,11 @@
+package June_2026;
+import java.util.Scanner;
+public class Returntype {
+    public static void main(String[] args) {
+        System.out.println(anuj());
+    }
+    public static String anuj(){
+        System.out.println("ReturnType");
+        return "Anuj";
+    }
+}

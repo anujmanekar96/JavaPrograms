5557d15a9529ed74eef36f06be980be4c687a093
Anuj Manekar <anuj.manekar@technia.com>
2026-07-09 22:21:18 +0530

    Literals

---
+++ src/July_2026/Literals.java
@@ -0,0 +1,13 @@
+
+
+public class Literals {
+    public static void main(String[] args) {
+        int age=34;
+        char ch='A';
+        float f=45.54f;
+        double d= 3452.43D;
+        System.out.println(age + ch + f + d );
+        System.out.println(ch + f + d );
+        System.out.println(age + ch + f + d );
+    }
+}
